4bc21acec1c0b89039081b885d2680c7c1205a64
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-02 15:34:17 +0300

    Build header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,22 +5,22 @@ import Logo from "../../images/logo.svg";
 
 const Header = () => {
   return (
-    <div className="">
-      <header>
+    <div className="container mx-auto flex-center">
+      <header className="flex w-screen">
         <div className="logo">
           <img src={Logo} alt="logo"></img>
         </div>
 
-        <div className="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
+        <div className="hamburger flex flex-col space-y-2 md:hidden">
+          <span className="inline-block w-10 h-1.5 bg-black"></span>
+          <span className="inline-block w-10 h-1.5 bg-black"></span>
+          <span className="inline-block w-10 h-1.5 bg-black"></span>
         </div>
 
-        <nav className="navbar">
+        <nav className="navbar hidden md:flex justify-between w-full">
           {/* List of items in the navigation bar */}
-          <div className="nav-list">
-            <ul>
+          <div className="nav-list w-full">
+            <ul className="flex space-x-5 md:ml-6">
               <li>Features</li>
               <li>Pricing</li>
               <li>Resources</li>
@@ -28,7 +28,7 @@ const Header = () => {
           </div>
 
           {/* Call to action */}
-          <div className="cta">
+          <div className="cta space-x-5 flex ">
             <button>Login</button>
             <button>Sign Up</button>
           </div>
