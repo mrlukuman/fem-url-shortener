4634ec6fdfb2bcfcdd6ad85fa2ae194349641d82
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-04 21:14:16 +0300

    Fix width and margin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,21 +5,21 @@ import Logo from "../../images/logo.svg";
 
 const Header = () => {
   return (
-    <div className="container mx-auto flex-center">
-      <header className="flex w-screen">
+    <div className="">
+      <header className="">
         <div className="logo">
           <img src={Logo} alt="logo"></img>
         </div>
 
         <div className="hamburger flex flex-col space-y-2 md:hidden">
-          <span className="inline-block w-10 h-1.5 bg-black"></span>
-          <span className="inline-block w-10 h-1.5 bg-black"></span>
-          <span className="inline-block w-10 h-1.5 bg-black"></span>
+          <span className="inline-block w-10 h-1.5 bg-black rounded"></span>
+          <span className="inline-block w-10 h-1.5 bg-black rounded"></span>
+          <span className="inline-block w-10 h-1.5 bg-black rounded"></span>
         </div>
 
-        <nav className="navbar hidden md:flex justify-between w-full">
+        <nav className="navbar hidden md:flex justify-between">
           {/* List of items in the navigation bar */}
-          <div className="nav-list w-full">
+          <div className="nav-list">
             <ul className="flex space-x-5 md:ml-6">
               <li>Features</li>
               <li>Pricing</li>
