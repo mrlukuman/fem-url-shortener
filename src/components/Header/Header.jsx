7c6c2a9b630a29a3a2ae4b741e48e509b86fc66c
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-04 21:59:21 +0300

    Fix Header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,20 +6,20 @@ import Logo from "../../images/logo.svg";
 const Header = () => {
   return (
     <div className="container mx-auto">
-      <header className="flex flec-col justify-between">
+      <header className="flex flec-col justify-between font-medium text-gray">
         <div className="logo flex-initial">
           <img src={Logo} alt="logo"></img>
         </div>
 
-        <div className="hamburger flex flex-col space-y-1.5 md:hidden">
-          <span className="inline-block w-10 h-1.5 bg-black rounded-sm"></span>
-          <span className="inline-block w-10 h-1.5 bg-black rounded-sm"></span>
-          <span className="inline-block w-10 h-1.5 bg-black rounded-sm"></span>
+        <div className="hamburger flex flex-col space-y-2 md:hidden ">
+          <span className="inline-block w-12 h-1.5 bg-black rounded-sm"></span>
+          <span className="inline-block w-12 h-1.5 bg-black rounded-sm"></span>
+          <span className="inline-block w-12 h-1.5 bg-black rounded-sm"></span>
         </div>
 
         <nav className="navbar flex-auto hidden md:flex justify-between md:ml-4">
           {/* List of items in the navigation bar */}
-          <div className="nav-list">
+          <div className="nav-list text-lg">
             <ul className="flex space-x-5 md:ml-6">
               <li>Features</li>
               <li>Pricing</li>
@@ -28,9 +28,11 @@ const Header = () => {
           </div>
 
           {/* Call to action */}
-          <div className="cta space-x-5 flex ">
+          <div className="cta space-x-5 flex">
             <button>Login</button>
-            <button>Sign Up</button>
+            <button className="px-4 py-2 text-white rounded-3xl bg-cyan">
+              Sign Up
+            </button>
           </div>
         </nav>
       </header>
