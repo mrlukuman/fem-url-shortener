18c3be9bbe67864f8bf08a0528aa43f504a62d09
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-05 19:08:55 +0300

    Add hero illustration

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import heroimage from "../../images/illustration-working.svg";
+
+const Hero = () => {
+  return (
+    <div className="container mx-auto">
+      <div></div>
+      <div>
+        <img src={heroimage} alt="heroimage" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
