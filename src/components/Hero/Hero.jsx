1dad790522e184f5463f4cebf09b48253af1a091
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-05 20:38:05 +0300

    Add hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,9 +2,16 @@ import heroimage from "../../images/illustration-working.svg";
 
 const Hero = () => {
   return (
-    <div className="container mx-auto">
-      <div></div>
-      <div>
+    <div className="container mx-auto flex">
+      <div className="hero-description flex-1">
+        <h1>More than just shorter links</h1>
+        <h5>
+          Build your brand’s recognition and get detailed insights on how your
+          links are performing.
+        </h5>
+        <button className="">Get Started</button>
+      </div>
+      <div className="hero-illustration flex-1">
         <img src={heroimage} alt="heroimage" />
       </div>
     </div>
