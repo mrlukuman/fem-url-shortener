861a8b7cf73db939833dc5219d573dbb855d71b2
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-06 17:48:59 +0300

    Add shorten section

--- src/components/shortner/Shortener.jsx
+++ src/components/shortner/Shortener.jsx
@@ -1,5 +1,12 @@
 const Shortener = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container mx-auto">
+      <div className="bg-[url('images/bg-shorten-desktop.svg')] bg-no-repeat">
+        <input type="text" placeholder="Shorten a link here..." />
+        <button className="p-40"> Shorten It!</button>
+      </div>
+    </div>
+  );
 };
 
 export default Shortener;
