cd6856b22697f741516451330a662b0b78946d18
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-07 17:08:23 +0300

    Add input form

--- src/components/shortner/Shortener.jsx
+++ src/components/shortner/Shortener.jsx
@@ -1,6 +1,6 @@
 const Shortener = () => {
   return (
-    <div className="container mx-auto my-6 p-4 bg-gray">
+    <div className="container mx-auto my-6 p-4 bg-gray rounded-lg">
       <div className="flex flex-col space-y-6">
         <input
           type="text"
