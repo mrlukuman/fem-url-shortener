02ff80c508ec9143e6e5a094c1c7943980d723b5
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-09 13:37:19 +0300

    Add CTA section

--- src/components/CTA/Cta.jsx
+++ src/components/CTA/Cta.jsx
@@ -1,6 +1,6 @@
 export default function CTA() {
   return (
-    <div>
+    <div className="my-2 p-10 space-y-4 text-white bg-violet-700">
       <h1 className="text-3xl">Boost your links today</h1>
       <button className="rounded-full p-4 bg-cyan">Get started</button>
     </div>
