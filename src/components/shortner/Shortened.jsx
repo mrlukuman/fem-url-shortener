c1f519421a0bc9ce138eb8657c6df50e9e7ea61c
Tajudeen Lukuman Aleru <74155780+tjalani@users.noreply.github.com>
2023-01-12 18:59:27 +0300

    Add shortened link component

---
+++ src/components/shortner/Shortened.jsx
@@ -0,0 +1,17 @@
+function Shortened() {
+  return (
+    <div>
+      <div className="">
+        <a href="">
+          <div>original link</div>
+        </a>
+        <a href="">
+          <div>shortened link</div>
+        </a>
+        <button>copy or copied</button>
+      </div>
+    </div>
+  );
+}
+
+export default Shortened;
